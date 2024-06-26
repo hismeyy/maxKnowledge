31ad03a31e0ed61f8ab4ff126bf00482ff899bfc
MaxCosmos <renyh2001@outlook.com>
2024-06-26 16:09:09 +0800

    完善 maxKnowledge 文档的配置
    - 修改config配置文件，规划nav导航
    - 创建关于作者，文档排版约定，下一步的计划，计算机组成原理md文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,33 @@ import {defineUserConfig} from 'vuepress/cli'
 import {viteBundler} from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-    lang: 'en-US',
+    lang: 'zh-CN',
 
-    title: 'VuePress',
-    description: 'My first VuePress Site',
+    title: 'MaxKnowledge',
+    description: 'MaxKnowledge Site',
 
     theme: defaultTheme({
         logo: 'https://vuejs.press/images/hero.png',
 
-        navbar: ['/', '/get-started'],
+        navbar: [
+            {text: '首页', link: '/'},
+            {
+                text: '入站必读',
+                children: [
+                    {text: '关于作者', link: '/入站必读/关于作者'},
+                    {text: '文档排版约定', link: '/入站必读/文档排版约定'},
+                    {text: '下一步的计划', link: '/入站必读/下一步的计划'},
+                ],
+            },
+            {
+                text: '计算机基础',
+                children: [
+                    {text: '计算机组成原理', link: '/计算机基础/计算机组成原理'},
+                    {text: '计算机网络', link: '/计算机基础/计算机网络'},
+                    {text: '操作系统', link: '/计算机基础/操作系统'},
+                ],
+            },
+        ],
     }),
 
     bundler: viteBundler(),
