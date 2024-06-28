a8f4e50f4a8a13bcd0cf8a1517a6348c0dcfa835
MaxCosmos <renyh2001@outlook.com>
2024-06-28 10:23:36 +0800

    修改配置，添加新的文章
    - 修改config配置文件，添加经验笔记/MySQL
    - 创建解决创建索引卡死的问题d文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,6 +27,12 @@ export default defineUserConfig({
                     {text: '计算机组成原理', link: '/计算机基础/计算机组成原理/'},
                 ],
             },
+            {
+              text: '经验笔记',
+              children: [
+                  {text: 'MySQL', link: '/经验笔记/MySQL/'},
+              ],
+            },
         ],
 
         sidebar: {
@@ -35,6 +41,14 @@ export default defineUserConfig({
                     text: '计算机组成原理',
                     children: [
                         {text: '计算机科学速成课', link: '/计算机基础/计算机组成原理/计算机科学速成课'},
+                    ],
+                },
+            ],
+            '/经验笔记/MySQL/': [
+                {
+                    text: '经验笔记',
+                    children: [
+                        {text: '解决创建索引卡死的问题', link: '/经验笔记/MySQL/解决创建索引卡死的问题'},
                     ],
                 },
             ],
