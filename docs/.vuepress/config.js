582e022fc640069b3b41ac3422fc4954a21b4782
MaxCosmos <renyh2001@outlook.com>
2024-07-03 19:46:38 +0800

    1. 修改文档布局信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ export default defineUserConfig({
                 children: [
                     {text: '关于作者', link: '/入站必读/关于作者'},
                     {text: '文档排版约定', link: '/入站必读/文档排版约定'},
-                    {text: '文档书写计划', link: '/入站必读/文档书写计划'},
+                    {text: '文档更新历史', link: '/入站必读/文档更新历史'},
                 ],
             },
             {
@@ -34,7 +34,7 @@ export default defineUserConfig({
                 {
                     text: '计算机组成原理',
                     children: [
-                        {text: '计算机科学速成课', link: '/计算机基础/计算机组成原理/计算机科学速成课'},
+                        {text: '计算机历史', link: '/计算机基础/计算机组成原理/计算机历史'},
                     ],
                 },
             ],
